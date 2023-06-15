9f1a423b0c4028ee7b90fc0f451f61d6ce0fc71a
Maphari <phumudzomaphari57@gmail.com>
2023-06-15 07:00:43 -0700

    size program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - prints the size of various types
+ * on the computer it is compiled and run on
+ * should produce the exact same output as in the example
  *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
 printf("Size of a char: %lu bytes(s)\n", sizeof(char));
