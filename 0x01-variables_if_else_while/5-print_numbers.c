e114a9b95ea4ed86077e2655c356cb29bca40a9b
Maphari <phumudzomaphari57@gmail.com>
2023-06-17 05:31:57 -0700

    c program that print base qo number starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 numbers
+ * from 0 - 9
+ *
+ * Retuen: Always 0
+ */
+
+int main(void)
+{
+	int baseTenNumbers;
+
+	for (baseTenNumbers = 0; baseTenNumbers < 10; baseTenNumbers++)
+	printf(baseTenNumbers);
+	printf("\n");
+
+	return (0);
+}
