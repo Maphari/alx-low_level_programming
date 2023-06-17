655d7f79d58523d732b6efad1a277e73afce73e1
Maphari <phumudzomaphari57@gmail.com>
2023-06-17 04:23:02 -0700

    c program that assing a new two random number everytime we run the program and we check the last then do condition based on that last number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -prints the last digit of n and prints out whether it * is greater than 5, 0, or less than 6
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	lastDigit = n % 10;
+	printf("Last digit of %d is %d ", n, lastDigit);
+       	if (lastDigit > 5)
+	printf("and is greater tha 5\n");
+	else if (lastDigit == 0)
+	printf("and is 0\n");
+	else if	(lastDigit < 6 && lastDigit != 0)
+	printf("and is less than 6 and not 0\n");
+	return (0);
+}
