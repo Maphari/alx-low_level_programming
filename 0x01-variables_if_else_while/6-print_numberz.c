284873fd1eb3f1e71c56add144c540a4a25fa34d
Maphari <phumudzomaphari57@gmail.com>
2023-06-17 05:42:33 -0700

    c program tha prints base 10 numbers only using putchar fuvtion and not allwed to use char types

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 number
+ * from 0 - 9
+ * using putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int baseTenNumbers;
+
+	for (baseTenNumbers = 0; baseTenNumbers < 10; baseTenNumbers++)
+	putchar(baseTenNumbers);
+	putchar('\n');
+
+	return (0);
+}
