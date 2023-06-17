58024f7d294dfd568f08189291f83557acf4c96c
Maphari <phumudzomaphari57@gmail.com>
2023-06-17 04:43:42 -0700

    c prgram tha prints A - Z in terminal using putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabets in lowecase
+ *
+ * Return : Always 0
+ */
+
+int main(void)
+{
+	char alphabets;
+
+	for(alphabets = 'A'; alphabets <= 'Z'; alphabets++)
+	putchar(alphabets);
+
+	return (0);
+}
