55475c6a75d4757f93ccdcb7bb44b3712ac44c7d
Maphari <phumudzomaphari57@gmail.com>
2023-06-20 05:28:15 -0700

    a funtion that prints alphabets in loer case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,18 +6,11 @@
  * Return: nothing, return void
  */
 
-void print_alphabet()
+void print_alphabet(void)
 {
         char letters;
 
         for (letters = 'a'; letters <= 'z'; letters++)
         _putchar(letters);
         _putchar('\n');
-}
-
-int main(void) 
-{
-	print_alphabet();
-
-	return (0);
 }
