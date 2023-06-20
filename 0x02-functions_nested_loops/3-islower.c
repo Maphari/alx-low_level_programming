c56ea8f91b4dcbd59963c4950802070fd61d93ee
Maphari <phumudzomaphari57@gmail.com>
2023-06-20 05:51:19 -0700

    checking if character is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - print alphabet
+ * check if those alphabets are in lowercase or not
+ *
+ * Return: Always 0.
+ *
+ */
+
+int _islower(int c)
+{
+if (islower(c))
+return (1);
+else
+return (0);
+}
