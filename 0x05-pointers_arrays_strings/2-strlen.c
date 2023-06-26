4c2dcc5dca7f6d4b66310cc21995501b97e12169
Maphari <phumudzomaphari57@gmail.com>
2023-06-26 04:30:01 -0700

    prototype function that returns s dtring length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns a string length
+ * @c: takes in a char arg
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	return strlen(*s);
+}
