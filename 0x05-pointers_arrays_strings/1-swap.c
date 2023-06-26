7d8e58477f7f286f3ba21d8387c93268fe5b20c1
Maphari <phumudzomaphari57@gmail.com>
2023-06-26 04:21:51 -0700

    a c prototype function that swaps two intergers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,9 +2,9 @@
 
 /**
  * swap_int - a function that swaps values
- *@a - takes and interger
- *@b - also takes in an integer
- * Return: void 
+ *@a: takes and interger
+ *@b: also takes in an integer
+ * Return: void
  */
 
 void swap_int(int *a, int *b)
